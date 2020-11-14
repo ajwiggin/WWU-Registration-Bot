c5e25dc034b3482bcfddbc3bf81571ea317566cc
Dan Aimone <danaimone@gmail.com>
2020-11-13 18:41:33 -0800

    cleaned up run_bot() moved getting user CRNs to user.py

--- frontend.py
+++ frontend.py
@@ -8,101 +8,52 @@ import os
 from user import User
 
 
+# TODO: define function, separate out smaller functions
+def run_bot(user):
+    # Set browser to Chrome
+    browser = webdriver.Chrome(user.os)
+
+    # Go to registration page
+    browser.get('https://admin.wwu.edu/pls/wwis/bwskfreg.P_AltPin')
+
+    # Find username entry box
+    username_entry = browser.find_element_by_xpath("""//*[@id="username"]""")
+    # Send username to username entry box
+    username_entry.send_keys(user.get_username())
+    # Find password entry box
+    password_entry = browser.find_element_by_xpath("""//*[@id="password"]""")
+    # Send password to password entry box
+    password_entry.send_keys(user.get_password())
+
+    # Find "login" button, save to var
+    login_button = browser.find_element_by_xpath("""//*[@id="fm1"]/section[4]/input[4]""")
+    # Click "login" button
+    login_button.click()
+
+    # Find Term drop down menu
+    termselect = Select(browser.find_element_by_xpath("""//*[@id="term_id"]"""))
+
+    submit_button = browser.find_element_by_xpath("""/html/body/div[3]/form/input""")
+    submit_button.click()
+
+    CRN_id_num = 1
+    for CRN in user.CRNs:
+        crn_entry = browser.find_element_by_xpath(f"""//*[@id="crn_id{CRN_id_num}"]""")
+        crn_entry.send_keys(CRN)
+        CRN_id_num += 1
+
+    submit_CRNs = browser.find_element_by_xpath("""/html/body/div[3]/form/input[19]""")
+    submit_CRNs.click()
+
+
 def main():
     # TODO: refactor this function
-    def crn():
-        global crn1, crn2, crn3, crn4, crn5
-        # Query user for CRN
-        crn1 = SG.PopupGetText('Please type your first desired CRN:', 'CRN 1')
-
-        # TODO: this could be simplified for sure
-        while len(crn1) != 5:
-            SG.Popup('This CRN is not valid. Please try again.')
-            crn1 = SG.PopupGetText('Please type your first desired CRN:', 'CRN 1')
-
-        crn2 = SG.PopupGetText('Please type your second desired CRN:', 'CRN 2')
-        while len(crn2) != 5:
-            SG.Popup('This CRN is not valid. Please try again.')
-            crn2 = SG.PopupGetText('Please type your second desired CRN:', 'CRN 2')
-
-        crn3 = SG.PopupGetText('Please type your third desired CRN:', 'CRN 3')
-        while len(crn3) != 5:
-            SG.Popup('This CRN is not valid. Please try again.')
-            crn3 = SG.PopupGetText('Please type your third desired CRN:', 'CRN 3')
-
-        crn4 = SG.PopupGetText('Please type your fourth desired CRN: (If unneccesary, type "none")',
-                               'CRN4')
-        if (len(crn4) != 5) and crn4 != "none":
-            SG.Popup('This CRN is not valid. Please try again.')
-            crn5 = SG.PopupGetText(
-                'Please type your fourth desired CRN: (If unneccesary, type "none")', 'CRN4')
-
-        crn5 = SG.PopupGetText('Please type your fifth desired CRN: (If unneccesary, type "none")',
-                               'CRN5')
-        if (len(crn5) != 5) and crn5 != "none":
-            SG.Popup('This CRN is not valid. Please try again.')
-            crn5 = SG.PopupGetText(
-                'Please type your fifth desired CRN: (If unneccesary, type "none")', 'CRN5')
-
-        # TODO: is this logging necessary?
-        # Print CRNs
-        print(crn1, crn2, crn3)
-        if crn4 is not "none":
-            print("CRN 4 not active.")
-        if crn5 is not "none":
-            print("CRN 5 not active.")
 
     user = User()
     cd = user.get_user_os()
 
-    # Run crn
-    crn()
-
-    # TODO: make run bot its own class or object seperated from front-end
-    def runbot():
-        # Set browser to Chrome
-        browser = webdriver.Chrome(cd)
-
-        # Go to registration page
-        browser.get('https://admin.wwu.edu/pls/wwis/bwskfreg.P_AltPin')
-
-        # Find username entry box
-        usernameentry = browser.find_element_by_xpath("""//*[@id="username"]""")
-        # Send username to username entry box
-        usernameentry.send_keys(user.get_username())
-        # Find password entry box
-        passwordentry = browser.find_element_by_xpath("""//*[@id="password"]""")
-        # Send password to password entry box
-        passwordentry.send_keys(user.get_password())
-
-        # Find "login" button, save to var
-        loginbutton = browser.find_element_by_xpath("""//*[@id="fm1"]/section[4]/input[4]""")
-        # Click "login" button
-        loginbutton.click()
-
-        # Find Term drop down menu
-        termselect = Select(browser.find_element_by_xpath("""//*[@id="term_id"]"""))
-
-        submitbutton = browser.find_element_by_xpath("""/html/body/div[3]/form/input""")
-        submitbutton.click()
-
-        # Insert CRNs for user
-        crnentry1 = browser.find_element_by_xpath("""//*[@id="crn_id1"]""")
-        crnentry1.send_keys(crn1)
-
-        crnentry2 = browser.find_element_by_xpath("""//*[@id="crn_id2"]""")
-        crnentry2.send_keys(crn2)
-
-        crnentry3 = browser.find_element_by_xpath("""//*[@id="crn_id3"]""")
-        crnentry3.send_keys(crn3)
-
-        # Click submit button
-        submitcrns = browser.find_element_by_xpath("""/html/body/div[3]/form/input[19]""")
-        submitcrns.click()
-
-    # Run runbot
-    runbot()
+    user.get_crn()
+    run_bot(user)
 
 
-# Run main
 main()
--- user.py
+++ user.py
@@ -2,10 +2,13 @@ import keyring
 import PySimpleGUI as SG
 import platform
 import os
+
+
 class User:
     username = ''
     password = ''
     os = ''
+    CRNs = []
     service_id = 'wwu-reg-app'
     USERNAME_KEY = 'cinema-voucher5-reattach'
 
@@ -22,6 +25,7 @@ class User:
 
     # TODO: check if this legacy code
     def get_user_os(self):
+        cd = ''
         user_os = platform.system()
         if "Darwin" in user_os:
             user_os = "Mac"
@@ -31,6 +35,25 @@ class User:
             cd = "chromedriverWindows"
 
         user_path = os.getcwd()
-        cd = str(user_path) + "/" + cd
+        cd += str(user_path) + "/"
 
         return user_os
+
+    def get_crn(self):
+        # Query user for CRN
+        # two conditions:
+        # crn is not none
+        # crn length is 5
+        i = 0
+        crn_num = i + 1
+        has_crn = True
+        while has_crn:
+                if i > 2:
+                    crn = SG.PopupGetText(f'Please type CRN {crn_num}, or none if you are done:',
+                                          f'CRN {crn_num}')
+                else:
+                    crn = SG.PopupGetText(f'Please type CRN {crn_num}:', f'CRN {crn_num}')
+
+                has_crn = ((len(crn) != 5) or ((crn is not "None") or (crn is not None)))
+                self.CRNs[i] = crn
+                i += 1
